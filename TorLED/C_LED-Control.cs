f14bdcf420ed103ddfc6db2ef10be7e32c0af3e6
Dennis Marx <dennis_marx@web.de>
2018-03-02 13:40:13 +0100

    Projektdateien hinzufügen.

---
+++ TorLED/C_LED-Control.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TorLED
+{
+    class C_LED_Control
+    {
+
+        #region variables
+
+        private String _host, _ledcode;
+        private int _port, _duration, _okcount=0, _okshouldbe = 2;
+
+
+        private Co0nUtilZ.C_NetClient tcpclient;
+        //private TorLED.C_NetClient tcpclient;
+        #endregion
+
+        #region methods
+
+        public void connect(int timeout=-1)
+        {
+            if (this.tcpclient!=null && !this.tcpclient.IsConnected)
+            {
+                if (timeout > 0)
+                {//Abweichender Timeout wurde angegeben und ist gültig
+                    this.tcpclient.ConnectTimeout = timeout; //...diesen setzen
+                }
+                this.tcpclient.ConnectToServer(); //Verbindung herstellen.
+            }          
+        }
+        #endregion
+
+        #region delegates_and_events
+        public delegate void Readyhandler();
+        public delegate void Errorhandler(object sender, Co0nUtilZ.ErrorEventArgs Args);
+        public delegate void Resulthandler(object sender, int returncode);
+        public delegate void Progresshandler(object sender, Co0nUtilZ.ProgressEventArgs Args);
+        public event Readyhandler ControllerReady;
+        public event Readyhandler Disconnected;
+        public event Resulthandler Jobdone;
+        public event Errorhandler ErrorOccured;
+        public event Progresshandler otherMessagereceived;
+        
+
+        #endregion
+
+        #region handlers
+
+        private void Handle_Connected(object sender, Co0nUtilZ.ProgressEventArgs e)
+        {
+            
+        }
+
+        private void Handle_Disconnected(object sender, Co0nUtilZ.ProgressEventArgs e)
+        {
+            if (this.Disconnected != null)
+            {
+                this.Disconnected();
+            }
+        }
+
+        private void Handle_DataReceived(object sender, Co0nUtilZ.ProgressEventArgs e)
+        {
+            
+            if (e.Message.StartsWith("LED-Control ready."))
+            {//Schritt eins
+                if (this.ControllerReady != null)
+                {
+                    this.ControllerReady();
+                }
+
+                if (this._okcount == 0)
+                {
+                    this.tcpclient.SendData(this._ledcode + "\r\n"); //Set LEDs on
+                }
+
+            }
+            else if (e.Message.StartsWith("command ok"))
+            {
+                this._okcount += 1;
+                if (this._okcount == 1)
+                { //Schritt zwei
+                    Thread.Sleep(this._duration);//Wait
+                    this.tcpclient.SendData("000\r\n"); //Set LEDS Off
+                }
+                else if (this._okcount == 2)
+                {
+                    Thread.Sleep(150); //Wait 150ms
+                    this.tcpclient.SendData("exit\r\n"); //Verbindung trennen
+                }
+            }
+            else if (e.Message.StartsWith("good bye"))
+            {
+                if (this.Jobdone != null)
+                {
+                this.Jobdone(this, this._okshouldbe - this._okcount); //0 wenn Erfolg                 
+                   
+                }
+
+                
+            }
+            else if (e.Message.StartsWith("command error"))
+            {//Fehler: ungültiger Befehl
+                if (this.ErrorOccured != null)
+                {
+                    this.ErrorOccured(this, new Co0nUtilZ.ErrorEventArgs("Error: " + e.Message));
+                }
+            }
+            else
+            {
+                if (this.otherMessagereceived != null)
+                {
+                    this.otherMessagereceived(this, new Co0nUtilZ.ProgressEventArgs(0, e.Message));
+                }
+
+               
+            }
+
+        }
+
+        private void Handle_DataSent(object sender, Co0nUtilZ.ProgressEventArgs e)
+        {
+
+
+        }
+
+        private void Handle_ErrorOccured(object sender, Co0nUtilZ.ErrorEventArgs e)
+        {
+           if (this.ErrorOccured != null)
+            {
+                this.ErrorOccured(this, e);
+            }
+        }
+
+        #endregion
+
+        #region contructor
+
+        public C_LED_Control(String Host, String LEDCODE, int Port, int Duration)
+        {
+
+            this._host = Host;
+            this._ledcode = LEDCODE;
+            this._port = Port;
+            this._duration = Duration;
+
+
+            this.tcpclient = new Co0nUtilZ.C_NetClient(this._host, this._port);
+            //this.tcpclient = new TorLED.C_NetClient(this._host, this._port);
+            this.tcpclient.ConnectTimeout = 1300; //Maximal x Millisekunden versuchen eine Verbindung aufzubauen.
+
+            //Events abbonieren
+            this.tcpclient.Connected += this.Handle_Connected;
+            this.tcpclient.Disconnected += this.Handle_Disconnected;
+            this.tcpclient.DataReceived += this.Handle_DataReceived;
+            this.tcpclient.DataSent += this.Handle_DataSent;
+            this.tcpclient.OnError += this.Handle_ErrorOccured;
+            
+        }
+
+        
+
+        #endregion
+    }
+}
---
+++ TorLED/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TorLED
+{
+    class Program
+    {
+
+        private static String Versioninfo = "Version: 0.2, 02.03.2018";
+
+        private static int returncode = -1;
+        private static bool jobdone = false;
+        
+
+        private static void Handle_Ready()
+        {
+            Console.WriteLine("\r\nVerbindung wurde hergestellt. - Controller ist bereit.");
+        }
+
+        private static void Handle_Disconnected()
+        {
+            Console.WriteLine("\r\nVerbindung wurde getrennt.");
+            jobdone = true;
+        }
+
+        private static void Handle_MsgReceived(object sender, Co0nUtilZ.ProgressEventArgs e)
+        {           
+           Console.WriteLine("\r\nUnerwartete Antwort: " + e.Message);           
+        }
+
+        private static void Handle_Jobdone(object sender, int result)
+        {
+            Console.WriteLine("\r\nAufgabe wurde beendet.");
+            Console.WriteLine("\r\nRückgabecode ist: " + result.ToString());
+            returncode = result;
+            jobdone = true;
+        }
+     
+
+        private static void Handle_ErrorOccured(object sender, Co0nUtilZ.ErrorEventArgs e)
+        {
+            Console.WriteLine("\r\nEs ist ein Fehler aufgetreten: " + e.Err);
+            jobdone = true;
+        }
+
+    
+        /// <summary>
+        /// Gibt die Hilfe aus
+        /// </summary>
+        static private void printHelp()
+        {
+            Console.WriteLine("\r\n");
+            Console.WriteLine("torledcontrol.exe by D. Marx");
+            Console.WriteLine("============================");
+            Console.WriteLine(Versioninfo);
+            Console.WriteLine("");
+            Console.WriteLine("Dieses Programm steuert die LED-Beleuchtung an einem Verladetor.");
+            Console.WriteLine("Dazu wird ein bestimmter Steuercode an eine (Raspberrypi-basierte)-Steuerung gesendet, auf welcher der passende TCP-Server (dmarxtcp) läuft.");
+            Console.WriteLine("");
+            Console.WriteLine("Aufruf:");
+            Console.WriteLine("#######");
+            Console.WriteLine("");
+            Console.WriteLine("torledcontrol.exe IP PORT LEDCODE DAUER [TIMEOUT]");
+            Console.WriteLine("");
+            Console.WriteLine("IP:\tIP-Adresse des Steuergeräts");
+            Console.WriteLine("PORT:\tTCP-Port des Steuergeräts");
+            Console.WriteLine("LEDCODE:\tSteuercode Rot,Grün,Blau");
+            Console.WriteLine("DAUER:\tLeuchtdauer des Steuercodes in Millisekunden (Minimum 250ms)");
+            Console.WriteLine("");
+            Console.WriteLine("Beispiel: torledcontrol.exe 192.168.14.124 60666 100 1500 3000");
+            Console.WriteLine("Erklärung: Dies versucht für 3000ms eine Verbindung zu 192.168.14.124:60666 aufzubauen. Bei Erfolg werden die LEDs anschließend für 1,5 Sekunden auf Rot gesetzt.");
+            Console.WriteLine("");
+            Console.WriteLine("LED-Codes:");
+            Console.WriteLine("##########");
+            Console.WriteLine("");
+            Console.WriteLine("Codes bestehen aus drei Bits, mit welchen sich verschiedene Farben darstellen lassen:");
+            Console.WriteLine("Erstes Bit: Rot");
+            Console.WriteLine("Zweites Bit: Grün");
+            Console.WriteLine("Drittes Bit: Blau");
+            Console.WriteLine("");
+            Console.WriteLine("Kombination:");
+            Console.WriteLine("000=Aus");
+            Console.WriteLine("100=Rot");
+            Console.WriteLine("010=Grün");
+            Console.WriteLine("001=Blau");
+            Console.WriteLine("110=Gelb");
+            Console.WriteLine("011=Türkis");
+            Console.WriteLine("101=Violett");
+            Console.WriteLine("111=Weiss");
+            Console.WriteLine("");
+            Console.WriteLine("TIMEOUT (Optional): Gibt an wie lange (in Millisekunden) versucht werden soll eine Verbindung herzustellen.");
+            Console.WriteLine("Wird kein Wert angegeben, wird der Standardwert verwendet.");
+        }
+
+        
+
+        static int Main(string[] args)
+        {
+
+
+        String _host, _ledcode;
+        int _port, _duration;
+
+            C_LED_Control _LED_Control;
+            if (args.Count() < 4)
+            {
+                printHelp();
+                return 6;
+            }
+            else if(args[2].Length != 3)
+            {
+                Console.WriteLine("\r\nUngültiger RGB-Code\r\n");
+                printHelp();
+                return 5;
+            }
+            else
+            {
+                try
+                {
+                   _duration = int.Parse(args[3]);
+                    if (_duration < 250)
+                    {
+                        Console.WriteLine("\r\nUngültige Dauer\r\n");
+                        return 3;
+                    }
+                    _port = int.Parse(args[1]);
+                    if (_port<=0 || _port > 65535)
+                    {
+                        Console.WriteLine("\r\nUngültiger Port\r\n");
+                        return 3;
+                    }
+                    _host = args[0];
+                    _ledcode = args[2];
+                }
+                catch
+                {
+                    printHelp();
+                    return 3;
+                }
+                
+
+                //Main:
+                try
+                {
+                    _LED_Control = new C_LED_Control(_host, _ledcode, _port, _duration); //Neues Objekt erzeugen...
+                    //...und konfigurieren
+                    _LED_Control.ControllerReady += Handle_Ready;
+                    _LED_Control.ErrorOccured += Handle_ErrorOccured;
+                    _LED_Control.otherMessagereceived += Handle_MsgReceived;
+                    _LED_Control.Jobdone += Handle_Jobdone;
+                    _LED_Control.Disconnected += Handle_Disconnected;
+                    int timeout = -1;
+
+                    if (args.Count() > 4)
+                    {
+                        //Fünftes Argument (Timeout) angegeben
+                        try
+                        {
+                            timeout = int.Parse(args[4]);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Ungültiger Wert für Timeout! - Verwende Standardwert...");                            
+                        }
+                        
+                    }
+                    if (timeout <= 0)
+                    {
+                        Console.WriteLine("Ungültiger Wert für Timeout! - Verwende Standardwert...");
+
+                    }
+                    
+                    _LED_Control.connect(timeout); //Verbindung herstellen. Wenn timeout <=0 ist, wird von der Klasse automatisch der Standardwert verwendet
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return 4;
+                }
+
+            }
+            
+            while (!jobdone)
+            {//Warten bis der Job (in welcher Form auch immer) beendet wurde...
+
+            }
+            return returncode; //Programm mit dem entsprechenden Rückgabecode beenden
+        }
+    }
+}
